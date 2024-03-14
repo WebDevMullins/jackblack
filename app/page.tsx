417d4846beeeef710024fb5327a06c05f7f71b55
WebDevMullins <webdevmullins@gmail.com>
2024-03-14 10:06:25 -0500

    add calculateHandValue, update Hit/Stand to show value, dealer down card turns over on Stand

--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,17 @@ import CardBack from "./card-back";
 import { CardProps, generateDeck, shuffleDeck } from "./deck";
 
 import { Button } from "@/components/ui/button";
-import calculateHandValue from "@/lib/calculateHandValue"
+import calculateHandValue from "@/lib/calculateHandValue";
 
 export default function HomePage() {
   const [deck, setDeck] = useState<CardProps[]>([]);
   const [dealerHand, setDealerHand] = useState<CardProps[]>([]);
   const [playerHand, setPlayerHand] = useState<CardProps[]>([]);
-  const [dealerHandValue, setDealerHandValue] = useState<CardProps[]>([]);
-  const [playerHandValue, setPlayerHandValue] = useState<CardProps[]>([]);
+  const [dealerHandValue, setDealerHandValue] = useState<number>();
+  const [playerHandValue, setPlayerHandValue] = useState<number>();
   const [gameStarted, setGameStarted] = useState(false);
   const [gameOver, setGameOver] = useState(false);
+  const [stand, setStand] = useState(false);
 
   const startGame = () => {
     const deck = shuffleDeck(generateDeck());
@@ -27,8 +28,9 @@ export default function HomePage() {
     setDealerHand(dealerHand);
     setGameStarted(true);
     setGameOver(false);
-    calculateHandValue(playerHand);
-
+    setStand(false);
+    setPlayerHandValue(calculateHandValue(playerHand));
+    setDealerHandValue(calculateHandValue(dealerHand));
   };
 
   const handleHit = () => {
@@ -37,31 +39,48 @@ export default function HomePage() {
       if (newCard) {
         setPlayerHand([...playerHand, newCard]);
         setDeck(deck);
+        setPlayerHandValue(calculateHandValue([...playerHand, newCard]));
       }
     }
   };
 
   const handleStand = () => {
     if (!gameOver) {
+      setStand(true);
       setGameOver(true);
     }
   };
 
   return (
-    <main className="bg-background dark flex min-h-screen flex-col items-center gap-y-16 p-24">
-      <h1 className="text-primary text-6xl font-bold">JackBlack</h1>
+    <main className="dark flex min-h-screen flex-col items-center gap-y-16 bg-background p-24">
+      <h1 className="text-6xl font-bold text-primary">JackBlack</h1>
       {gameStarted ? (
         <div className="flex flex-col items-center justify-center gap-y-24">
           <div className="flex flex-col items-center justify-center gap-y-4">
-            <h2 className="text-primary text-xl">Dealer Cards:</h2>
+            <h2 className="text-xl text-primary">Dealer</h2>
+            {stand ? (
+              <h3 className="text-primary">{dealerHandValue}</h3>
+            ) : (
+              <h3 className="text-primary">
+                {calculateHandValue([dealerHand[0]])}
+              </h3>
+            )}
             <div className="flex gap-2">
-              <Card suit={dealerHand[0].suit} value={dealerHand[0].value} />
-              {/* Display only the first card of the dealer */}
-              <CardBack />
+              {stand ? (
+                dealerHand.map((card, index) => (
+                  <Card key={index} suit={card.suit} value={card.value} />
+                ))
+              ) : (
+                <>
+                  <Card suit={dealerHand[0].suit} value={dealerHand[0].value} />
+                  <CardBack />
+                </>
+              )}
             </div>
           </div>
           <div className="flex flex-col items-center justify-center gap-y-4">
-            <h2 className="text-primary text-xl">Player Cards:</h2>
+            <h2 className="text-xl text-primary">Player</h2>
+            <h3 className="text-primary">{playerHandValue}</h3>
             <div className="flex flex-wrap gap-2">
               {playerHand.map((card, index) => (
                 <Card key={index} suit={card.suit} value={card.value} />
